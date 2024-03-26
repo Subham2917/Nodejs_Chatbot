cf371b34cd4b743b9a31b29fb50fda5666158412
subhamsingh <msubham017@gmail.com>
2024-03-27 00:09:28 +0530

    Nodejs Chatbot

---
+++ config/open-ai.js
@@ -0,0 +1,14 @@
+import OpenAI  from 'openai';
+// import dotenv from 'dotenv';
+// dotenv.config();
+
+const openai = new OpenAI({
+  apiKey: "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",
+});
+
+
+
+// const openai = new OpenAIApi(configuration);
+
+
+export default openai ;
